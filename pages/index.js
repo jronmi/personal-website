d4e8849998930bc698d558772493f8d099022ccc
James <jronmi@gmail.com>
2023-07-25 21:04:14 -0400

    change default pattern

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,104 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>James Richardson's Website</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+			<main>
+				<header>
+					<img src="/public/favicon.ico"></img>
+					<h1 className={styles.title}>James Richardson</h1>
+				</header>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+				<p className={styles.description}>
+					Hello, I'm James. I'm a Graduate Computer Science Student at MIT with a passion for Machine
+					Learning. This website is a platform for me to display my interests
+				</p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+				<div className={styles.grid}>
+					<Link href="/Reading/" className={styles.card}>
+						<h3>Reading List &rarr;</h3>
+						<p>A set of academic papers which I recommend</p>
+					</Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+					<Link href="/projects/" className={styles.card}>
+						<h3>Projects &rarr;</h3>
+						<p>I compiled my projects for anyone to view</p>
+					</Link>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+					<Link href="/Resources/" className={styles.card}>
+						<h3>Resources &rarr;</h3>
+						<p>Here are some resources for people learning Machine Learning</p>
+					</Link>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+					{/* <Link href="/Blog/" className={styles.card}>
+						<h3>Blog &rarr;</h3>
+						<p></p>
+					</Link> */}
+				</div>
+			</main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+			<footer>
+				<a href="https://github.com/jronmi" target="_blank" rel="noopener noreferrer">
+					<img src="/favicon.ico" alt="Vercel" className={styles.logo} />
+				</a>
+			</footer>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
+			<style jsx>{`
+				main {
+					padding: 5rem 0;
+					flex: 1;
+					display: flex;
+					flex-direction: column;
+					justify-content: center;
+					align-items: center;
+				}
+				footer {
+					width: 100%;
+					height: 100px;
+					border-top: 1px solid #eaeaea;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				}
+				footer img {
+					margin-left: 0.5rem;
+				}
+				footer a {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					text-decoration: none;
+					color: inherit;
+				}
+				code {
+					background: #fafafa;
+					border-radius: 5px;
+					padding: 0.75rem;
+					font-size: 1.1rem;
+					font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+						Bitstream Vera Sans Mono, Courier New, monospace;
+				}
+			`}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+			<style jsx global>{`
+				html,
+				body {
+					padding: 0;
+					margin: 0;
+					font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
+						Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+				}
+				* {
+					box-sizing: border-box;
+				}
+			`}</style>
+		</div>
+	);
 }
